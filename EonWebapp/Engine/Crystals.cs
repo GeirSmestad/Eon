123c8a29b9c893fab78e238f21098d2275a9ab4c
Smestad, Geir <Geir.Smestad@sbanken.no>
2019-08-02 16:36:08 +0200

    Added some basic game engine entities

---
+++ EonWebapp/Engine/Crystals.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EonWebapp.Engine
+{
+    public class Crystals
+    {
+        public int Yellow { get; set; }
+
+        public int Green { get; set; }
+
+        public int Blue { get; set; }
+
+        public int Pink { get; set; }
+
+        public bool ContainsAmount(Crystals amount)
+        {
+            return
+                this.Yellow >= amount.Yellow &&
+                this.Green >= amount.Green &&
+                this.Blue >= amount.Blue &&
+                this.Pink >= amount.Pink;
+        }
+
+        public bool ContainsMoreThanEightCrystals()
+        {
+            return Yellow + Green + Blue + Pink > 8;
+        }
+
+        public Crystals AddAmountTo(Crystals amountToAdd)
+        {
+            return new Crystals()
+            {
+                Yellow = this.Yellow + amountToAdd.Yellow,
+                Green = this.Green + amountToAdd.Green,
+                Blue = this.Blue + amountToAdd.Blue,
+                Pink = this.Pink + amountToAdd.Pink
+            };
+        }
+
+        public Crystals RemoveAmountFrom(Crystals amountToRemove)
+        {
+            if (!this.ContainsAmount(amountToRemove))
+            {
+                throw new InvalidOperationException("Not sufficient crystals to remove specified amount");
+            }
+
+            return new Crystals()
+            {
+                Yellow = this.Yellow - amountToRemove.Yellow,
+                Green = this.Green - amountToRemove.Green,
+                Blue = this.Blue - amountToRemove.Blue,
+                Pink = this.Pink - amountToRemove.Pink
+            };
+        }
+    }
+}
---
+++ EonWebapp/Engine/PlayerState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EonWebapp.Engine
+{
+    public class PlayerState
+    {
+        Crystals Funds { get; set; }
+        // List<GameCard> Cards {get;set;}
+
+        // ExtraPointTokens ExtraPoints { get;set; }
+        // List<VictoryCard> VictoryCards { get;set; }
+    }
+}
